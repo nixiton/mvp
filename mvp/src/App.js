36256dfcbe7c646172a65412619c07909064294c
test <ny.andriambolamalala@gmail.com>
2022-06-04 12:07:59 +0200

    init

---
+++ mvp/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+import React, { useState} from "react";
+
+
+
+const App = () => {
+
+  const [count, setCount] = useState(false);
+
+  const handleClickCount = () => {
+    //console.log("io");
+     setCount(count + 1);
+  };
+
+  const handleClickReset = () => {
+    //console.log("io");
+     setCount(0);
+  };
+
+  return (
+
+    <div className="App">
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+      </head>
+      <label ><h1>{count}</h1></label><br></br>
+
+      <button className="btn btn-primary" onClick={() =>handleClickCount()}>Count</button>
+      <button className="btn btn-primary" onClick={() =>handleClickReset()}>Reset</button>
+
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    </div>
+
+
+  );
+};
+
+export default App;
